bd8960f8dfa749ffc9494e2a32834adc798e70f5
Isabella Hu <eunicejhu@gmail.com>
2020-11-17 11:28:02 +0800

    fix: errors

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export enum Theme {
-  Dark,
-  Light,
-}
-
+export type Theme = "dark" | "light";
+export const THEME = {
+  DARK: "dark",
+  LIGHT: "light",
+};
 const ThemeContext = React.createContext({
-  theme: Theme.Dark,
+  theme: "dark",
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setTheme: (theme: Theme): void => {},
 });
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 import history from "./helpers/history";
-import ThemeContext, { Theme } from "./context/ThemeContext";
+import ThemeContext, { THEME } from "./context/ThemeContext";
 import store from "./store/index";
 
 import App from "./App";
 import "./index.css";
 
 const Entry = () => {
-  const [theme, setTheme] = useState(Theme.Dark);
+  const [theme, setTheme] = useState(THEME.DARK);
   return (
     <Provider store={store}>
       <ThemeContext.Provider value={{ theme, setTheme }}>
