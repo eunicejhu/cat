366a05cb8f9bbb32467cd63576845dd607176004
Isabella Hu <eunicejhu@gmail.com>
2020-12-04 01:19:55 +0800

    feat: add routes to Navbar
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import {
+    LANDING,
+    NOTIFICATIONS,
+    SIGN_IN,
+    HOME,
+    ACCOUNT,
+    ADMIN,
+} from "./constants/routes";
+
 const NavBar = () => (
-  <nav>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/notifications">Notifications</Link>
-    </li>
-  </nav>
+    <nav>
+        <li>
+            <Link to={LANDING}>Landing</Link>
+        </li>
+        <li>
+            <Link to={SIGN_IN}>Sign In</Link>
+        </li>
+        <li>
+            <Link to={HOME}>Home</Link>
+        </li>
+        <li>
+            <Link to={ACCOUNT}>Account</Link>
+        </li>
+        <li>
+            <Link to={ADMIN}>Admin</Link>
+        </li>
+        <li>
+            <Link to={NOTIFICATIONS}>Notifications</Link>
+        </li>
+    </nav>
 );
 
 export default NavBar;
