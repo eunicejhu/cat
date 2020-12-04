8280332e13c6c5b699ee5d981b085008507613a9
Isabella Hu <eunicejhu@gmail.com>
2020-12-04 23:55:39 +0800

    feat: add SignUp route
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,24 +5,30 @@ import PostsList from "./features/posts/PostsList";
 import EditPostForm from "./features/posts/EditPostForm";
 import NotificationsList from "./features/notifications/NotificationsList";
 
+import * as ROUTES from "./constants/routes";
+import SignUpPage from "./components/SignUp";
+
 const NoMatch = () => <>No match</>;
 const Routes = () => (
-  <Switch>
-    <Route exact path="/">
-      <PostsList />
-    </Route>
-    <Route exact path="/posts/:id">
-      <SinglePostPage />
-    </Route>
-    <Route exact path="/editPost/:id">
-      <EditPostForm />
-    </Route>
-    <Route exact path="/notifications/">
-      <NotificationsList />
-    </Route>
-    <Route>
-      <NoMatch />
-    </Route>
-  </Switch>
+    <Switch>
+        <Route exact path="/">
+            <PostsList />
+        </Route>
+        <Route exact path="/posts/:id">
+            <SinglePostPage />
+        </Route>
+        <Route exact path={ROUTES.SIGN_UP}>
+            <SignUpPage />
+        </Route>
+        <Route exact path="/editPost/:id">
+            <EditPostForm />
+        </Route>
+        <Route exact path="/notifications/">
+            <NotificationsList />
+        </Route>
+        <Route>
+            <NoMatch />
+        </Route>
+    </Switch>
 );
 export default Routes;
