e54cc6804f7d619daf1942c35ebc949344db3380
Isabella Hu <eunicejhu@gmail.com>
2020-12-20 05:59:58 +0800

    test: fix  NavBar test & snapshot
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/NavBar.test.tsx
+++ src/NavBar.test.tsx
@@ -6,29 +6,29 @@ import { Provider } from "react-redux";
 import store from "./store/index";
 
 const withBrowserRouterAndStore = (ui: React.ReactNode, route = "/initial") => {
-  window.history.pushState({}, "", route);
-  return (
-    <BrowserRouter>
-      <Provider store={store}>{ui}</Provider>
-    </BrowserRouter>
-  );
+    window.history.pushState({}, "", route);
+    return (
+        <BrowserRouter>
+            <Provider store={store}>{ui}</Provider>
+        </BrowserRouter>
+    );
 };
 test("click Home, location pathname set to /", () => {
-  const ui = withBrowserRouterAndStore(<NavBar />);
-  render(ui);
-  fireEvent.click(screen.getByText(/Home/i));
-  expect(window.location.pathname).toBe("/");
+    const ui = withBrowserRouterAndStore(<NavBar />);
+    render(ui);
+    fireEvent.click(screen.getByText(/Home/i));
+    expect(window.location.pathname).toBe("/home");
 });
 test("click Notifications, location pathname set to /notifications", () => {
-  const ui = withBrowserRouterAndStore(<NavBar />);
-  render(ui);
-  fireEvent.click(screen.getByText(/Notifications/i));
-  expect(window.location.pathname).toBe("/notifications");
+    const ui = withBrowserRouterAndStore(<NavBar />);
+    render(ui);
+    fireEvent.click(screen.getByText(/Notifications/i));
+    expect(window.location.pathname).toBe("/notifications");
 });
 
 test("renders correctly", () => {
-  const ui = withBrowserRouterAndStore(<NavBar />);
-  const { asFragment } = render(ui);
+    const ui = withBrowserRouterAndStore(<NavBar />);
+    const { asFragment } = render(ui);
 
-  expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
 });
