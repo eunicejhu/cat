a19a42b34e93b5e1e3bf610e1840f28ded78476f
Isabella Hu <eunicejhu@gmail.com>
2020-12-27 00:13:38 +0800

    feat: pagination (add new selectors)
    Signed-off-by: Isabella Hu <eunicejhu@gmail.com>

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -6,9 +6,13 @@ import { User } from "../features/users/usersSlice";
 
 export const selectAllPosts = (state: State): Post[] => state.posts.data;
 export const selectPostById = (
-  state: State,
-  postId: string
+    state: State,
+    postId: string
 ): Post | undefined => state.posts.data.find((post) => post.id === postId);
 export const selectAllNotifications = (state: State): Notification[] =>
-  state.notifications;
+    state.notifications.items;
+export const selectNotificationsNextPage = (state: State): number =>
+    state.notifications.nextPage;
+export const selectNotificationsLimit = (state: State): number =>
+    state.notifications.limit;
 export const selectAllUsers = (state: State): User[] => state.users.data;
